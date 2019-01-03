9295527c3c936016210ab20fc74477ba160b6074
Taposh Das <taposh24x7@gmail.com>
2019-01-03 10:50:41 +0600

    Take city, month and day input from users and handle invalid input

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -16,14 +16,37 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
+
+    month = "all"
+    day = "all"
+    while True:
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-
-
+        city = input("Would you like to see data for Chicago, New York or Washington?\n")
+        city=city.title()
+        if city != "Chicago" and city != "New York" and city != "Washington":
+            print("\nYou must choose one city or restart the program if you don't want to continue")
+        else:
+            while True:
     # get user input for month (all, january, february, ... , june)
-
-
+                month_or_day = input("\nWould you like to filter your data based on month, day or not at all? Type 'none' if you don't want to add time filter.\n")
+                month_or_day = month_or_day.lower()#REVW 1
+                if month_or_day == "month":
+                    month = input("\nEnter name of the month\n").lower()
+                    if month=="january" or month=="february" or month=="march" or month=="april" or month=="may" or month=="june" or month=="july" or month=="august" or month=="september" or month=="october" or month=="november" or month=="december":
+                        break
+                    else:
+                        print("\nInvalid month name was given. Please give correct month name next time.")
     # get user input for day of week (all, monday, tuesday, ... sunday)
-
+                elif month_or_day == "day":
+                    day = input("\nFor which day of the week would you like to see the data? Type full name of day or type 'all' for all days of the week.\n")
+                    day = day.title()#REVW 1
+                    if day=="Monday" or day=="Tuesday" or day=="Wednesday" or day=="Thursday" or day=="Friday" or day=="Saturday":
+                        break
+                    else:
+                        print("\nInvalid name of day was given. Please give correct name of day next time.")
+                elif month_or_day == "none":
+                    break
+            break
 
     print('-'*40)
     return city, month, day
